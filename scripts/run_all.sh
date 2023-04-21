5c11a24f005d65370062637822815f604b8c5aab
Tom Silver <tomssilver@gmail.com>
2023-04-20 22:22:16 -0400

    minor

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -1,6 +1,6 @@
 # Script to reproduce all results, assuming responses from GPT are cached.
 
-for SEED in {1..9}
+for SEED in {0..9}
 do
     # Main approach.
     python llm_genplan/main.py --env pg3-manygripper --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
