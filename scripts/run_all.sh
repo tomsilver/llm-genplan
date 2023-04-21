b7f677b17a1a64d5a0c50a6feb9e9c8cc48d8382
Tom Silver <tomssilver@gmail.com>
2023-04-21 07:52:01 -0400

    fix

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -12,10 +12,10 @@ do
 
     # Ablate interactive debugging.
     python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-manygripper --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
-    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-manyferry --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
-    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-manymiconic --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
-    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-spannerlearning --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
-    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-hiking --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 2 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
-    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-trapnewspapers --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 3 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
+    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-manyferry --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
+    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-manymiconic --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
+    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-spannerlearning --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
+    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-hiking --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 2 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
+    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-trapnewspapers --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 3 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
 
 done
