28c23ce4cd0c0b8f442a3408af5e682248be3ebb
Tom Silver <tomssilver@gmail.com>
2023-04-25 15:50:22 -0400

    add heavypack to run file

--- scripts/run_all.sh
+++ scripts/run_all.sh
@@ -9,6 +9,7 @@ do
     python llm_genplan/main.py --env pg3-spannerlearning --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
     python llm_genplan/main.py --env pg3-hiking --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 2 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
     python llm_genplan/main.py --env pg3-trapnewspapers --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 3 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
+    python llm_genplan/main.py --env pg3-heavypack --seed $SEED --experiment_id chatgpt4 --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
 
     # Ablate interactive debugging.
     python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-manygripper --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
@@ -17,6 +18,7 @@ do
     python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-spannerlearning --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
     python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-hiking --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 2 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
     python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-trapnewspapers --seed $SEED --num_prompt_tasks 2 --num_extra_train_tasks 3 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
+    python llm_genplan/main.py --experiment_id chatgpt4-no-debug --load_experiment_id chatgpt4 --max_debug_attempts 0 --env pg3-heavypack --seed $SEED  --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache
 
     # Ablate chain-of-thought.
     python llm_genplan/main.py --env pg3-manygripper --seed $SEED --experiment_id chatgpt4-no-cot --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache --skip_chain_of_thought
@@ -25,5 +27,6 @@ do
     python llm_genplan/main.py --env pg3-spannerlearning --seed $SEED --experiment_id chatgpt4-no-cot --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache --skip_chain_of_thought
     python llm_genplan/main.py --env pg3-hiking --seed $SEED --experiment_id chatgpt4-no-cot --num_prompt_tasks 2 --num_extra_train_tasks 2 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache --skip_chain_of_thought
     python llm_genplan/main.py --env pg3-trapnewspapers --seed $SEED --experiment_id chatgpt4-no-cot --num_prompt_tasks 2 --num_extra_train_tasks 3 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache --skip_chain_of_thought
+    python llm_genplan/main.py --env pg3-heavypack --seed $SEED --experiment_id chatgpt4-no-cot --num_prompt_tasks 2 --num_extra_train_tasks 8 --num_eval_tasks 30 --abbreviate_problem_strs --force_load_from_cache --skip_chain_of_thought
 
 done
